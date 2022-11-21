98fefe9f4097a41af01e46e21314e5fcdccca6e8
Thib Seldon <71655828+ThibSeldon@users.noreply.github.com>
2022-11-21 09:57:24 +0100

    ajout des sections

--- Module_02/Cours/code.js
+++ Module_02/Cours/code.js
@@ -8,4 +8,42 @@ const findElement = (array, thingToFind) => {
  return false;
  }
 
- const myArray;
+ const myArray = ["thihabud", "Yvon", "Zlana"];
+
+ const result = findElement(myArray, "Yvon");
+ console.log(result);
+
+
+ function recur(n) {
+    if(n > 1) {
+        return n * (recur(n-1));
+    }
+    else {
+        return 1;
+    }
+ }
+
+
+ console.log(recur(6));
+
+ let moyennes = [10,23,1,23,4343,23,23,45,451,587,455];
+
+ let moyennes2 = [10,10];
+
+ const totalArray = moyennes.reduce((accumulator , value) => {
+    return accumulator + value;
+ },0);
+
+ console.log(totalArray);
+
+
+ function calculeMoyenne(moyenne) {
+    let total = 0;
+    for(let m of moyenne ) {
+    total += m;
+    }
+    return total / moyenne.length;
+}
+
+console.log(calculeMoyenne(moyennes));
+console.log(calculeMoyenne(moyennes2));
